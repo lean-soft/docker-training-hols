46682e64a76755605ddac3324a3c8d5ac701be31
Lei Xu <leixu@lean-soft.cn>
2017-01-26 06:12:30 +0000

    Added file install-sphinx.sh

---
+++ install-sphinx.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install -y python-pip python-dev build-essential 
+sudo pip install --upgrade pip 
+sudo pip install --upgrade virtualenv 
+pip install Sphinx
+pip install sphinx_rtd_theme
